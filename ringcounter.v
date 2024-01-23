975e71e8e08ced7562bfb6a1f3462574395a6ea6
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-01-23 10:28:34 +0530

    Create ringcounter.v

---
+++ ringcounter.v
@@ -0,0 +1,108 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj_example #(
+    parameter BITS = 32
+)(
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+    // Wishbone Slave ports (WB MI A)
+    input wb_clk_i,
+    input wb_rst_i,
+    input wbs_stb_i,
+    input wbs_cyc_i,
+    input wbs_we_i,
+    input [3:0] wbs_sel_i,
+    input [31:0] wbs_dat_i,
+    input [31:0] wbs_adr_i,
+    output wbs_ack_o,
+    output [31:0] wbs_dat_o,
+
+    // Logic Analyzer Signals
+    input  [127:0] la_data_in,
+    output [127:0] la_data_out,
+    input  [127:0] la_oenb,
+
+    // IOs
+    input  [`MPRJ_IO_PADS-1:0] io_in,
+    output [`MPRJ_IO_PADS-1:0] io_out,
+    output [`MPRJ_IO_PADS-1:0] io_oeb,
+
+    // IRQ
+    output [2:0] irq
+);
+    
+
+    /*wire [`MPRJ_IO_PADS-1:0] io_in;
+    wire [`MPRJ_IO_PADS-1:0] io_out;
+    wire [`MPRJ_IO_PADS-1:0] io_oeb;*/
+    wire Clock;
+    wire Reset;
+    wire[3:0] Count_out;
+    assign Clock=wb_clk_i;
+    assign io_out=Count_out;
+    assign {Reset}=io_in[`MPRJ_IO_PADS-2:0];
+    ring_counter u1 (Clock,Reset,Count_out);
+    
+endmodule
+
+   module ring_counter(
+    Clock,
+    Reset,
+    Count_out
+    );
+    input Clock;
+    input Reset;
+    output [3:0] Count_out;
+    reg [3:0] Count_temp;
+    always @(posedge (Clock), Reset)
+    begin
+        if(Reset == 1'b1)   begin  //when Reset is high 
+            Count_temp = 4'b0001;   end  //The Count value is reset to "0001".
+        else if(Clock == 1'b1)  begin  //When the Clock is high
+            //Left shift the Count value.
+            Count_temp = {Count_temp[2:0],Count_temp[3]};   end 
+    end
+    
+    //The Count value is assigned to final output port.
+    assign Count_out = Count_temp;
+    
+endmodule
+`default_nettype wire
