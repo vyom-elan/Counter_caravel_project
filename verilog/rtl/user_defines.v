1e75029aec951eb7320154cfedaae0bb1db571c2
Vyom <97853828+vyom-elan@users.noreply.github.com>
2024-01-25 12:47:35 +0530

    Update user_defines.v

--- verilog/rtl/user_defines.v
+++ verilog/rtl/user_defines.v
@@ -1,37 +1,16 @@
-// SPDX-FileCopyrightText: 2022 Efabless Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// SPDX-License-Identifier: Apache-2.0
-
 `default_nettype none
-
 `ifndef __USER_DEFINES_H
-// User GPIO initial configuration parameters
 `define __USER_DEFINES_H
-
 // deliberately erroneous placeholder value; user required to config GPIO's to other
 `define GPIO_MODE_INVALID                  13'hXXXX
-
 // Authoritive source of these MODE defs is: caravel/verilog/rtl/user_defines.v
 // Useful GPIO mode values.  These match the names used in defs.h.
-//
 `define GPIO_MODE_MGMT_STD_INPUT_NOPULL    13'h0403
 `define GPIO_MODE_MGMT_STD_INPUT_PULLDOWN  13'h0c01
 `define GPIO_MODE_MGMT_STD_INPUT_PULLUP    13'h0801
 `define GPIO_MODE_MGMT_STD_OUTPUT          13'h1809
 `define GPIO_MODE_MGMT_STD_BIDIRECTIONAL   13'h1801
 `define GPIO_MODE_MGMT_STD_ANALOG          13'h000b
-
 `define GPIO_MODE_USER_STD_INPUT_NOPULL    13'h0402
 `define GPIO_MODE_USER_STD_INPUT_PULLDOWN  13'h0c00
 `define GPIO_MODE_USER_STD_INPUT_PULLUP    13'h0800
@@ -39,7 +18,6 @@
 `define GPIO_MODE_USER_STD_BIDIRECTIONAL   13'h1800
 `define GPIO_MODE_USER_STD_OUT_MONITORED   13'h1802
 `define GPIO_MODE_USER_STD_ANALOG          13'h000a
-
 // The power-on configuration for GPIO 0 to 4 is fixed and cannot be
 // modified (allowing the SPI and debug to always be accessible unless
 // overridden by a flash program).
